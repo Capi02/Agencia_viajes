517055fdc2e564d392f249a2febcdaa0ec12eadb
Alejandro Capitanachi <users@correo.com>
2023-05-15 18:27:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import router from "./routes/index.js";
+import db from  "./config/db.js";
+
+const app = express();
+
+// CONECTAR LA BASE DE DATOS
+db.authenticate()
+    .then( () => console.log("Base de datos conectada"))
+    .catch( error => console.log(error));
+
+// DEFINIR ROUTER
+const port = process.env.PORT || 4000;
+
+// HABILITAR PUG
+app.set("view engine", "pug");
+
+// OBTENER EL AÑO ACTUAL
+app.use( (req, res, next) => {
+    const year = new Date();
+    res.locals.ActualYear = year.getFullYear(); 
+    res.locals.NombreSitio = "Agencia de Viajes";
+    next();
+})
+
+// DEFINIR LA CARPETA PUBLICA
+app.use(express.static("public"));
+
+// AGREGAR ROUTER
+app.use("/", router)
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+})
